abc1a8c9d873342b3f870a289f0852eac70bfa61
Steven Aerts <stevenaerts@me.com>
2015-10-07 11:24:02 +0200

    add minify module

--- html/sites/all/themes/gulp/gulpfile.js
+++ html/sites/all/themes/gulp/gulpfile.js
@@ -1,3 +1,8 @@
 /**
  * Created by saerts2013 on 7/10/15.
  */
+var gulp        = require('gulp'),
+	browserSync = require('browser-sync'),
+	less        = require('gulp-less'),
+	prefix      = require('gulp-autoprefixer'),
+	shell       = require('gulp-shell');
